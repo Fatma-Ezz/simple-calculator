df7d52a2474ea10838fc32b115db92ae8a2f3a6b
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-02-14 07:06:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-function calculate(operation) {
+let operation = '';
+
+function setOperation(op) {
+    operation = op;
+}
+
+function calculate() {
     var num1 = parseFloat(document.getElementById('num1').value);
-    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultBox = document.getElementById('result');
     var result;
 
-    if (isNaN(num1) || isNaN(num2)) {
-        result = "Please enter valid numbers.";
+    if (isNaN(num1)) {
+        result = "Please enter a valid number.";
     } else {
         switch (operation) {
             case 'add':
-                result = num1 + num2;
+                result = num1 + num1; // Example logic, can be replaced
                 break;
             case 'subtract':
-                result = num1 - num2;
+                result = num1 - num1; // Example logic, can be replaced
                 break;
             case 'multiply':
-                result = num1 * num2;
+                result = num1 * num1; // Example logic, can be replaced
                 break;
             case 'divide':
-                if (num2 === 0) {
-                    result = "Cannot divide by zero.";
-                } else {
-                    result = num1 / num2;
-                }
+                result = num1 !== 0 ? num1 / num1 : "Cannot divide by zero."; // Example logic, can be replaced
+                break;
+            default:
+                result = "Please select an operation.";
                 break;
         }
     }
 
-    document.getElementById('result').innerText = result;
+    resultBox.value = result;
 }
